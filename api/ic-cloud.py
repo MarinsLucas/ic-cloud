5efee63590af343e6408a051160de8bb825176fc
MarinsLucas <lucasmarinsramalho@gmail.com>
2023-09-10 21:52:49 -0300

    acho que deu certo?

--- api/ic-cloud.py
+++ api/ic-cloud.py
@@ -3,8 +3,13 @@ import requests
 import os
 import pyvista
 from io import StringIO
- 
+import shutil
+
+
 app = Flask(__name__)
+static_image_path = os.path.join('static', 'images')
+app.config['UPLOAD_FOLDER'] = static_image_path
+
 
 #faz o "get"
 def make_request(secret_message, url):
@@ -12,10 +17,13 @@ def make_request(secret_message, url):
     url = url+"get_data"
     try:
         PARAMS = {'value': 2, 'message' :secret_message}
-        response = requests.get(url, params=PARAMS)
+        response = requests.get(url, params=PARAMS, stream=True)
         if response.status_code == 200:
-            data = response.json()
-            return data['message']
+            #data = response.json()
+            with open("temp.vtu", 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            return #data#['message']
+        
         else:
             print("Erro na requisição:", response.status_code)
     except requests.exceptions.RequestException as e:
@@ -24,22 +32,24 @@ def make_request(secret_message, url):
 @app.route('/')
 def form():
     return render_template('form.html')
- 
+
 @app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    form_data = request.form
+    print(form_data.getlist('mensagem'))
+    make_request(form_data['mensagem'], str(form_data['url']))
+    reader = pyvista.read("temp.vtu")
+    filename = 'cabo.jpeg'
+    filepath = os.path.join(static_image_path, filename)
+    reader.plot(off_screen=True, window_size=(500,500), screenshot=filepath)
+    print('/' + filepath.replace("\\", "/"))
+    return render_template('data.html', image_url = os.path.join(app.config['UPLOAD_FOLDER'], filename)) #os.path.join('imagens', filename)
+
+""" @app.route('/data/', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
-        form_data = request.form
-        print(form_data.getlist('mensagem'))
-        filetext = make_request(form_data['mensagem'], str(form_data['url']))
-        f = open("temp.vtu", "w")
-        f.write(filetext)
-        reader = pyvista.read("temp.vtu")
-        reader.plot(off_screen=True, window_size=(500,500), screenshot=filepath)
-        static_image_path = os.path.join('static', 'images')
-        filename = 'cabo.png'
-        filepath = os.path.join(static_image_path, filename)
-        return os.path.join('imagens', filename)
+        return render_template("data.html") """
  
 app.run(host='localhost', port=5000, debug=True)
